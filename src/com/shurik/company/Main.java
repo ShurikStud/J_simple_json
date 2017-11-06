cb79ea7c910bfd178b5913b83692fd0e6b847ea5
note-5i\shurik <abashev.alexandr@gmail.com>
2017-11-06 20:29:46 +0200

    init

---
+++ src/com/shurik/company/Main.java
@@ -0,0 +1,11 @@
+package com.shurik.company;
+
+import org.json.simple.JSONObject;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JSONObject object   = new JSONObject();
+
+    }
+}
